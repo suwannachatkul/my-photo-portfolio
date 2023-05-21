84e9fd904025dbadb18d17f2672c01bea4b52f21
saran.su <suwannachatkul.s@gmail.com>
2023-05-21 22:47:05 +0900

    Update Route in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import "./App.css";
+import ComingSoonPage from "./pages/ComingSoon";
+import ContactPage from "./pages/Contact";
 import ErrorPage from "./pages/Error";
 import GalleryPage, { loader as GalleryLoader } from "./pages/Gallery";
+import GalleryRoot from "./pages/GalleryRoot";
+import GalleryUpload from "./pages/GalleryUpload";
 import HomePage, { loader as HomeLoader } from "./pages/Home";
-import ContactPage from "./pages/Contact";
-
+// import LoginPage from "./pages/Login";
+import ProtectedRoutes from "./pages/ProtectedRoute";
 
 const router = createBrowserRouter([
   {
@@ -17,8 +22,21 @@ const router = createBrowserRouter([
       { path: "home", element: <HomePage /> },
     ],
   },
-  { path: "gallery", element: <GalleryPage />, loader: GalleryLoader },
+  {
+    path: "gallery",
+    element: <GalleryRoot />,
+    children: [
+      { index: true, element: <GalleryPage />, loader: GalleryLoader },
+      {
+        path: "upload",
+        element: <ProtectedRoutes />,
+        children: [{ index: true, element: <GalleryUpload /> }],
+      },
+    ],
+  },
   { path: "contact", element: <ContactPage /> },
+  { path: "blog", element: <ComingSoonPage /> },
+  { path: "*", element: <ErrorPage /> },
 ]);
 
 function App() {
