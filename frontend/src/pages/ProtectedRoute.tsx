d735402ab2078007176575662fa0d5f92ca8d4ab
saran.su <suwannachatkul.s@gmail.com>
2023-06-22 19:44:54 +0900

    Update ProtectedRoute to wait auth response

--- frontend/src/pages/ProtectedRoute.tsx
+++ frontend/src/pages/ProtectedRoute.tsx
@@ -1,13 +1,15 @@
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 import {
+  stateAuthProcessState,
   stateAuthToken,
   stateIsAuth,
   refreshAccessToken,
 } from "../store/authSlice";
 import { useAppDispatch } from "../store/store";
+import LoadingFullPage from "../components/UI/LoadingFullPage";
 
 const ProtectedRoutes = () => {
   const dispatch = useAppDispatch();
@@ -18,12 +20,25 @@ const ProtectedRoutes = () => {
   const location = useLocation();
   const isAuth = useSelector(stateIsAuth);
   const token = useSelector(stateAuthToken);
+  const authProcessState = useSelector(stateAuthProcessState);
+
+  if (authProcessState === 'pending' || authProcessState === 'init' ) {
+    throw Promise.resolve(); // Throw a promise to suspend rendering
+  }
 
   return isAuth && token ? (
-    <Outlet />
-  ) : (
-    <Navigate to="/Error" replace state={{ from: location }} />
+      <Outlet />
+    ) : (
+      <Navigate to="/Error" replace state={{ from: location }} />
+    );
+};
+
+const ProtectedRoutesWrapper = () => {
+  return (
+    <Suspense fallback={<LoadingFullPage />}>
+      <ProtectedRoutes />
+    </Suspense>
   );
 };
 
-export default ProtectedRoutes;
+export default ProtectedRoutesWrapper;
