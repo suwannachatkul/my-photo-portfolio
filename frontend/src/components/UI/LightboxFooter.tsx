2148d5e72753a446ad366fcfac169b9332e0e8b7
saran.su <suwannachatkul.s@gmail.com>
2023-08-06 10:15:04 +0900

    Add delay on showing Lightbox footer

--- frontend/src/components/UI/LightboxFooter.tsx
+++ frontend/src/components/UI/LightboxFooter.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { LightboxGalleryItem } from "./Lightbox";
 import styles from "./Lightbox.module.css";
 
@@ -7,11 +8,23 @@ interface ILightboxFooter {
 }
 
 const LightboxFooter = (props: ILightboxFooter) => {
+  const [shouldHide, setShouldHide] = useState(true);
+
+  useEffect(() => {
+    // Initially hide for 750ms
+    const timer = setTimeout(() => {
+      setShouldHide(false);
+    }, 750);
+
+    // Clear the timer on unmount
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
       <footer
         className={`row gx-0 px-4 py-2 py-xl-4 ${styles.footer} ${
-          props.isIdle && styles.hide
+          shouldHide || props.isIdle ? styles.hide : ''
         }`}
       >
         <h2 className="mx-4 my-2 my-xl-4">{props.image.title}</h2>
