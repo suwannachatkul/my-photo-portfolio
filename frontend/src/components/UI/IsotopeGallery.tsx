6e6c4c173cd332b166db0687736e9f1ed2bac40d
saran.su <suwannachatkul.s@gmail.com>
2023-10-21 20:14:21 +0900

    Add LazyLoading transition css

--- frontend/src/components/UI/IsotopeGallery.tsx
+++ frontend/src/components/UI/IsotopeGallery.tsx
@@ -7,7 +7,7 @@ import {
   useState,
 } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/blur.css";
+import "./LazyloadAnimate.css"
 
 import Isotope from "isotope-layout";
 
